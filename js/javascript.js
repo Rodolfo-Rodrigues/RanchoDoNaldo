ee02ed745dad1dd9419325c8cf55640f20a4fa3f
Rodolfo-Rodrigues <50324906+Rodolfo-Rodrigues@users.noreply.github.com>
2020-08-07 08:00:42 -0300

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -1,16 +1,27 @@
 let showMenu = document.querySelector('.show_menu').addEventListener('click', clicar)
-function clicar(){
-	let menu = document.querySelector('#menu ul')
-	menu.classList.toggle('active')
+
+function clicar() {
+    let menu = document.querySelector('#menu ul')
+    menu.classList.toggle('active')
 }
 
-onload  = start;
+onload = start;
 
-function start(){	
-var i = 1;
-function Move(){ 	
-	i = (i%4)+1; // 5 is the Number of image in slider
-	document.getElementById('i'+i).checked = true;
+function start() {
+    var i = 1;
+
+    function Move() {
+        i = (i % 2) + 1; // 2 is the Number of image in slider
+        document.getElementById('i' + i).checked = true;
+    }
+    setInterval(Move, 15000); //change img in 15 sec
 }
-setInterval(Move,10000); //change img in 10 sec
-}
+/*
+const element = document.querySelector('.animate');
+element.classList.add('animate__animated', 'animate__pulse', 'animate__infinite');
+element.style.setProperty('--animate-duration', '1.5s');
+
+const element2 = document.querySelector('.animate2');
+element2.classList.add('animate__animated', 'animate__pulse', 'animate__infinite');
+element2.style.setProperty('--animate-duration', '1.5s');
+*/
